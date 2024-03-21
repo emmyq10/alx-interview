04c3cbc02c22e4cde6551f2213bcc60755f5e480
emmyq10 <emmatunde2001@gmail.com>
2024-03-21 15:14:50 -0700

    task done completely

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -10,5 +10,3 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-carrie@ubuntu:~/0x02-minoperations$
